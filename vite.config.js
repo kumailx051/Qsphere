d25b49d743f42bfc0b519ca6da365ec58037f635
Kumail <kumailx051@gmail.com>
2026-05-13 22:53:13 +0500

    Fix: Make base path conditional for dev and production environments

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Qsphere/',
+  base: process.env.NODE_ENV === 'production' ? '/Qsphere/' : '/',
   plugins: [react()],
 })
